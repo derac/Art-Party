1191cfe8af7d59bd5f7ec2795f5b401fe61d6915
Derac <dwolson123@gmail.com>
2020-02-17 14:01:41 -0600

    UI tweak

--- Scripts/Setup/By_IP_Button.gd
+++ Scripts/Setup/By_IP_Button.gd
@@ -1,18 +1,21 @@
 extends Button
 
-onready var UDP_Players = get_node("/root/Setup/UDP_Players")
+onready var UDP_Players = get_node("../UDP_Players")
+onready var Name = get_node("../Name")
 
 func _pressed():
 	if text == "cancel":
 		if UDP_Players:
-			UDP_Players.rect_size = Vector2(1490,840)
-			UDP_Players.rect_position = Vector2(20,220)
+			UDP_Players.rect_size.y = 840
+			UDP_Players.rect_position.y = 220
+			Name.rect_position.y = 20
 			UDP_Players._on_udp_data_changed()
 		text = "by IP"
 	else:
 		if UDP_Players:
-			UDP_Players.rect_size = Vector2(1490,640)
-			UDP_Players.rect_position = Vector2(20,420)
+			UDP_Players.rect_size.y = 640
+			UDP_Players.rect_position.y = 420
+			Name.rect_position.y = 220
 			UDP_Players._on_udp_data_changed()
 		text = "cancel"
 	#block.set_visible(!block.is_visible())
