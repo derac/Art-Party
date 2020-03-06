237bdfa7b863b4498160a9592be84a699d610565
Derac <dwolson123@gmail.com>
2020-03-05 19:53:52 -0600

    Exit menu update

--- Scripts/0_Global/Exit_Menu/Background.gd
+++ Scripts/0_Global/Exit_Menu/Background.gd
@@ -1,25 +1,31 @@
-extends CanvasLayer
+extends Button
 
 func _notification(what) -> void:
 	if (what == MainLoop.NOTIFICATION_WM_QUIT_REQUEST or \
 		what == MainLoop.NOTIFICATION_WM_GO_BACK_REQUEST):
-		toggle_panel()
+		toggle()
 
 # Esc (default cancel key) to exit
 func _input(event) -> void:
 	if event.is_action_pressed("ui_cancel"):
-		toggle_panel()
+		toggle()
 
-func toggle_panel() -> void:
-	if $Panel.is_visible():
+func _pressed() -> void:
+	close()
+	
+func close() -> void:
+	$Menu/Quit_Button.set_visible(false)
+	$Menu/Menu_Button.set_visible(false)
+	visible = false
+
+func toggle() -> void:
+	if visible:
 		Sound.play_sfx("res://Assets/SFX/off.wav")
-		$Panel.set_visible(false)
-		$Panel/Menu/Exit_Button.set_visible(false)
-		$Panel/Menu/Menu_Button.set_visible(false)
+		close()
 	else:
 		Sound.play_sfx("res://Assets/SFX/on.wav")
 		if get_tree().get_current_scene().get_name() != "Setup":
-			$Panel/Menu/Menu_Button.set_visible(true)
+			$Menu/Menu_Button.set_visible(true)
 		else:
-			$Panel/Menu/Exit_Button.set_visible(true)
-		$Panel.set_visible(true)
+			$Menu/Quit_Button.set_visible(true)
+		visible = true
--- Scripts/0_Global/Exit_Menu/Menu_Button.gd
+++ Scripts/0_Global/Exit_Menu/Menu_Button.gd
@@ -5,5 +5,5 @@ onready var Setup_Screen = preload("res://Screens/Setup.tscn")
 func _pressed() -> void:
 	Sound.play_sfx("res://Assets/SFX/button1.wav", 0.0, 0.5)
 	get_tree().change_scene_to(Setup_Screen)
-	get_node("../Exit_Button").set_visible(true)
+	get_node("../Quit_Button").set_visible(true)
 	visible = false
