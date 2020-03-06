4447cc636dce113f80576e24b65189a97e46f238
Derac <dwolson123@gmail.com>
2020-03-05 18:18:45 -0600

    Fix bugs

--- Scripts/4_End/Review/Return_Button.gd
+++ Scripts/4_End/Review/Return_Button.gd
@@ -1,9 +1,9 @@
 extends Button
 
-onready var Canvas := get_node('/root/Review/Canvas')
+onready var Canvas := get_node('../../Canvas')
 
 func _pressed() -> void:
 	Sound.play_sfx("res://Assets/SFX/button2.wav", -3.0, 0.5)
-	get_parent().set_visible(false)
+	get_node('../../../Review').set_visible(false)
 	Canvas.history = [[]]
 	Canvas.redraw()
