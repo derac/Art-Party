f64e139f0489ea98c802d33750c2c927d82bb7d6
Derac <dwolson123@gmail.com>
2020-03-07 14:31:46 -0600

    Improve Review screen
    Added a UI element to show which element the player worked on for their turn.

--- Scripts/4_End/Review/Review.gd
+++ Scripts/4_End/Review/Review.gd
@@ -5,6 +5,8 @@ var display_turn := 1
 var scores := {}
 
 const score_game = preload("res://Scripts/Utility/score_game.gd")
+const Turn_Is_Canvas_stylebox = preload("res://Screens/Components/Turn_Is_Canvas.tres")
+const Word_stylebox = preload("res://Screens/Components/Review_Word.tres")
 
 func set_player_id(value : int) -> void:
 	player_id = value
@@ -30,21 +32,31 @@ func _on_Return_pressed():
 func update_display(turn : int) -> void:
 	display_turn = turn
 	var played_by = Global.game_state[player_id]["played_by"][turn]
+	$Controls/Turn.text = String(turn) + ". " + Global.game_state[played_by]["name"]
+	
 	if scores[played_by]:
+		Turn_Is_Canvas_stylebox.set_border_color(Color("#008751"))
+		
 		Sound.play_sfx("res://Assets/SFX/good.wav")
 		$Controls/Points.text = "+" + String(scores[played_by])
 		$Controls/Points.set_visible(true)
 	else:
+		Turn_Is_Canvas_stylebox.set_border_color(Color("#FF004D"))
+		
 		Sound.play_sfx("res://Assets/SFX/bad.wav", -3, .75)
 		$Controls/Points.set_visible(false)
-	$Controls/Turn.text = String(turn) + ". " + Global.game_state[played_by]["name"]
-
+	
 	if turn % 2:
+		Word_stylebox.set_border_color(Color("#83769C"))
+		$Turn_Is_Canvas.visible = true
+		
 		$Canvas.history = Global.game_state[player_id]["cards"][turn]
 		$Canvas.redraw()
 		$Controls/Word.text = Global.game_state[player_id]["cards"][turn - 1]
 	else:
+		if scores[played_by]:
+			Word_stylebox.set_border_color(Color("#008751"))
+		else:
+			Word_stylebox.set_border_color(Color("#FF004D"))
+		$Turn_Is_Canvas.visible = false
 		$Controls/Word.text = Global.game_state[player_id]["cards"][turn]
-
-
-
