52834110cd7317b1143cb9605d4ed45d072cbd9c
Derac <dwolson123@gmail.com>
2020-03-10 16:01:05 -0500

    Delete Menu_Button.gd

--- Classes/4_End/Menu_Button.gd
+++
@@ -1,9 +0,0 @@
-extends Button
-
-const setup_screen = preload("res://Screens/Setup.tscn")
-		
-func _pressed() -> void:
-	Sound.play_sfx("res://Assets/SFX/button1.wav", 0.0, 0.5)
-	var error : int = get_tree().change_scene_to(setup_screen)
-	if error:
-		print("Failed to change scene to setup_screen")
