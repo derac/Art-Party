61e9ef6eff6f784910465fc2ee5270cadf5a7ce3
Derac <dwolson123@gmail.com>
2020-03-09 19:46:19 -0500

    Add test data

--- Classes/1_Setup/UDP_Players.gd
+++ Classes/1_Setup/UDP_Players.gd
@@ -9,6 +9,15 @@ func _ready() -> void:
 func _on_udp_data_changed() -> void:
 	for child in get_children():
 		child.queue_free()
+
+#	var udp_data = {"1": {"is_server": true, "name": "derek", "ip": "1", "port": "1"},
+#				   "2": {"is_server": false, "name": "bob"},
+#				   "3": {"is_server": false, "name": "race"},
+#				   "4": {"is_server": false, "name": "jeff"},
+#				   "5": {"is_server": false, "name": "cody"},}
+#	for player in udp_data.keys():
+#		create_player_label(udp_data[player], player)
+	
 	for player in Global.udp_data.keys():
 		create_player_label(Global.udp_data[player], player)
 
