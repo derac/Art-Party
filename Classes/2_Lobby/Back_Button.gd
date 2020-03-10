1791b208181ef7303f72136f03f91ec7b62fbd66
Derac <dwolson123@gmail.com>
2020-03-10 16:02:39 -0500

    Refactor out back button to menu button

--- Classes/2_Lobby/Back_Button.gd
+++
@@ -1,7 +0,0 @@
-extends Button
-
-var setup_screen := load("res://Screens/Setup.tscn")
-		
-func _pressed() -> void:
-	Sound.play_sfx("res://Assets/SFX/button1.wav", 0.0, 0.5)
-	get_tree().change_scene_to(setup_screen)
